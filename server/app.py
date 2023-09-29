12cfd634d6ec73d9c17e73e2cba80f9153c25636
gustavowolfmann <gwolfmann@gmail.com>
2023-09-29 18:52:21 -0300

    meal plan functions

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
-from .models import Ingredient, Recipe
-from .routes import ingredients_router,recipes_router
+from .models import Ingredient, Recipe, MealPlan
+from .routes import ingredients_router,recipes_router, meal_router
 
 app = FastAPI()
 
@@ -11,9 +11,10 @@ async def start_beanie():
     # CREATE MOTOR CLIENT
     client = AsyncIOMotorClient( "mongodb://localhost:27017/dei0" )
 
-    await init_beanie(client.dei0, document_models=[Ingredient, Recipe])
+    await init_beanie(client.dei0, document_models=[Ingredient, Recipe, MealPlan])
     app.include_router(ingredients_router, prefix="/meals", tags=["ingredient"])
     app.include_router(recipes_router, prefix="/meals", tags=["recipes"])
+    app.include_router(meal_router, prefix="/meals", tags=["meal_plan"])
 
 @app.get("/", tags=["Root"])
 async def index() -> dict:
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
-from beanie import Document, Indexed
-from pydantic import BaseModel
+from beanie import Document, Indexed#, Link
+from pydantic import BaseModel, validator
 from enum import Enum
 from typing import Optional, List
 
@@ -26,12 +26,60 @@ class IngredientInRecipe(BaseModel):
     measure_unit: MeasureUnit
     
 class Recipe(Document):
-    name: Indexed(str)
+    name: Indexed(str,unique=True)
     ingredients: List[IngredientInRecipe]
     preparation: str
+
+async def get_recipe_by_name(name: str) -> Recipe:
+    recipe = await Recipe.find_one(Recipe.name == name)
+    if recipe is None:
+        return None
+    return recipe
 
 class Statuses(str, Enum):
     DELETED = "DELETED"
 
 class StatusModel(BaseModel):
     status: Statuses
+
+class DayOfWeek(str,Enum):
+    MO = 'mo'
+    TU = 'tu'
+    WE = 'we'
+    TH = 'th'
+    FR = 'fr'
+    SA = 'sa'
+    SU = 'su'
+
+class KindOfMeal(str,Enum):
+    BR = 'br'
+    LU = 'lu'
+    DI = 'di'
+    MM = 'mm'  #midle morning
+    MA = 'ma'  #midle afternoon
+    OT = 'ot'  #other
+
+class FoodInMealPlan(BaseModel):
+    portion : float
+    measure_unit : MeasureUnit
+    recipe_name : str
+    kom : KindOfMeal
+    dow : Optional[DayOfWeek] = None
+
+def foods_dict_to_list(foods_dict: dict) -> List[FoodInMealPlan]:
+    foods = []
+    for food in foods_dict:
+        food_model = FoodInMealPlan(
+            portion=food["portion"],
+            measure_unit=food["measure_unit"],
+            recipe_name=food["recipe_name"],
+            kom=KindOfMeal(food["kom"]),
+            dow=DayOfWeek(food["dow"]) if food["dow"] is not None else None,
+        )
+        foods.append(food_model)
+    return foods
+
+class MealPlan(Document):
+    name: Indexed(str, unique=True)
+    foods: Optional[List[FoodInMealPlan]]
+    obser: str
