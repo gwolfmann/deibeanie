e129cb61c85085bbe828e6c00087a7f3e660506a
gustavowolfmann <gwolfmann@gmail.com>
2023-10-03 13:28:27 -0300

    reorder the methods consistently

--- server/routes.py
+++ server/routes.py
@@ -16,18 +16,23 @@ async def get_ingredient(ingredient_id: PydanticObjectId) -> Ingredient:
         raise HTTPException(status_code=404, detail="ingredient not found")
     return ingredient
 
-# CRUD
+#### INGREDIENTS
+@ingredients_router.get("/ingredients/{ingredient_id}", response_model=Ingredient)
+async def get_ingredient_by_id(ingredient: Ingredient = Depends(get_ingredient)):
+    return ingredient
+
+@ingredients_router.get("/ingredients/", response_model=List[Ingredient])
+async def get_all_ingredients():
+    return await Ingredient.find_all().to_list()
+
+@ingredients_router.get("/ingredients/by_uuid/{uuid}", response_model=List[Ingredient])
+async def filter_notes_by_uuid(uuid: str):
+    return await Ingredient.find_many({"uuid": uuid}).to_list()
 
 @ingredients_router.post("/ingredients/", response_model=Ingredient)
 async def create_ingredient(ingredient: Ingredient):
     await ingredient.create()
     return ingredient
-
-
-@ingredients_router.get("/ingredients/{ingredient_id}", response_model=Ingredient)
-async def get_ingredient_by_id(ingredient: Ingredient = Depends(get_ingredient)):
-    return ingredient
-
 
 @ingredients_router.put("/ingredients/{ingredient_id}", response_model=Ingredient)
 async def update_ingredient(ingredient_data: Ingredient, ingredient: Ingredient = Depends(get_ingredient)):
@@ -37,22 +42,10 @@ async def update_ingredient(ingredient_data: Ingredient, ingredient: Ingredient 
     ingredient_updated = await ingredient.update({"$set": fields_to_update})
     return ingredient_updated
 
-
 @ingredients_router.delete("/ingredients/{ingredient_id}", response_model=StatusModel)
 async def delete_ingredient(ingredient: Ingredient = Depends(get_ingredient)):
     await ingredient.delete()
     return StatusModel(status=Statuses.DELETED)
-
-# LISTS
-
-@ingredients_router.get("/ingredients/", response_model=List[Ingredient])
-async def get_all_ingredients():
-    return await Ingredient.find_all().to_list()
-
-
-@ingredients_router.get("/ingredients/by_uuid/{uuid}", response_model=List[Ingredient])
-async def filter_notes_by_uuid(uuid: str):
-    return await Ingredient.find_many({"uuid": uuid}).to_list()
 
 ##### RECIPES ####
 
@@ -63,13 +56,21 @@ async def get_recipe(recipe_id: PydanticObjectId) -> Recipe:
         raise HTTPException(status_code=404, detail="recipe not found")
     return recipe
 
+#get a recipe
+@recipes_router.get("/recipes/{recipe_id}", response_model=Recipe)
+async def get_recipe_by_id(recipe: Recipe = Depends(get_recipe)):
+    return recipe
+
+# Get all recipes
+@recipes_router.get("/recipes/", response_model=List[Recipe])
+async def get_all_recipes():
+    return await Recipe.find_all().to_list()
 
 # Create a new recipe
 @recipes_router.post("/recipes/", response_model=Recipe)
 async def create_recipe(recipe: Recipe):
     await recipe.create()
     return recipe
-
 
 # Update a recipe
 @recipes_router.put("/recipes/{recipe_id}", response_model=Recipe)
@@ -94,16 +95,6 @@ async def update_recipe(recipe_id: PydanticObjectId, recipe_data: Dict[str, Any]
 async def delete_recipe(recipe: Recipe = Depends(get_recipe)):
     await recipe.delete()
     return StatusModel(status=Statuses.DELETED)
-
-#get a recipe
-@recipes_router.get("/recipes/{recipe_id}", response_model=Recipe)
-async def get_recipe_by_id(recipe: Recipe = Depends(get_recipe)):
-    return recipe
-
-# Get all recipes
-@recipes_router.get("/recipes/", response_model=List[Recipe])
-async def get_all_recipes():
-    return await Recipe.find_all().to_list()
 
 #### MEALS ####
 
@@ -142,12 +133,6 @@ async def create_meal_plan(meal_plan: MealPlan):
     await meal_plan.create()
     return meal_plan
 
-# Delete a meal plan
-@meal_router.delete("/mealplans/{meal_plan_id}", response_model=StatusModel)
-async def delete_meal_plan(meal_plan: MealPlan = Depends(get_meal_plan)):
-    await meal_plan.delete()
-    return StatusModel(status=Statuses.DELETED)
-
 #Put a meal plan updating
 @meal_router.put("/mealplans/{meal_plan_id}/update", response_model=MealPlan)
 async def update_meal_plan(meal_plan_id: PydanticObjectId, meal_plan_data: Dict[str, Any]):
@@ -169,3 +154,10 @@ async def add_meal_plan(meal_plan_id: PydanticObjectId, meal_plan_data: Dict[str
             raise HTTPException(status_code=404, detail="meal plan contains a recipe undefined")
     await meal_plan.update({"$push": {"foods": {"$each": foods_dict_to_list(meal_plan_data.get("foods"))}}})
     return meal_plan
+
+# Delete a meal plan
+@meal_router.delete("/mealplans/{meal_plan_id}", response_model=StatusModel)
+async def delete_meal_plan(meal_plan: MealPlan = Depends(get_meal_plan)):
+    await meal_plan.delete()
+    return StatusModel(status=Statuses.DELETED)
+
